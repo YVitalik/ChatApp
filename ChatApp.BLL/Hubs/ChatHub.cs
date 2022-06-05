9c5fc5f6ed7655ae0c106a310a02e66dac3dda93
VitalikYuzefovych <yuzefovychvitalii@gmail.com>
2022-06-05 15:40:47 +0300

    Small fixes to SignalR

--- ChatApp.BLL/Hubs/ChatHub.cs
+++ ChatApp.BLL/Hubs/ChatHub.cs
@@ -5,9 +5,14 @@ namespace ChatApp.BLL.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task JoinRoom(int chatId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+        }
+
         public async Task SendMessage(int chatId, Message message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.Group(chatId.ToString()).SendAsync("ReceiveMessage", message);
         }
     }
 }
